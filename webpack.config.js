332e075ff7de392fec8251840b966ff8fcefe46b
Amruta Patil <amrutapatil1720@gmail.com>
2020-01-02 20:29:33 -0800

    setup the environment and configured webpack

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require("path");
+const SRC_DIR = path.resolve(__dirname, "./client/src/components/");
+
+module.exports = {
+  entry: "./client/src/components/index.jsx",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "./client/dist")
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$|\.m?jsx$/,
+        include: SRC_DIR,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+            plugins: [["@babel/plugin-proposal-class-properties"]]
+          }
+        }
+      }
+    ]
+  }
+};
